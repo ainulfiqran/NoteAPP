a1d0d0db269cd303bc35916f0432fce8c0a6bf29
Akhmad Ainul Fiqran Rosady <fiqranrosady@gmail.com>
2021-05-26 22:28:29 +0800

    first commit

---
+++ app/src/main/java/id/ac/unhas/noteapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.ac.unhas.noteapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
